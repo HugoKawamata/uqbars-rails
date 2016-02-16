d67290485daa04ff4ccabc85606c6f822d0922b5
HugoKawamata <hugokawamata@gmail.com>
2016-02-16 15:34:31 +1000

    Add exec profiles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,9 +3,27 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( profiles/victor.jpg )
+Rails.application.config.assets.precompile += %w( profiles/brodie.jpg )
+Rails.application.config.assets.precompile += %w( profiles/hugo.jpg )
+Rails.application.config.assets.precompile += %w( profiles/dan.jpg )
+Rails.application.config.assets.precompile += %w( profiles/jim.jpg )
+Rails.application.config.assets.precompile += %w( profiles/hamish.jpg )
+Rails.application.config.assets.precompile += %w( profiles/kyle.jpg )
+Rails.application.config.assets.precompile += %w( profiles/alli.jpg )
+Rails.application.config.assets.precompile += %w( profiles/mitch.jpg )
+Rails.application.config.assets.precompile += %w( profiles/astrella.jpg )
+Rails.application.config.assets.precompile += %w( profiles/michael.jpg )
+Rails.application.config.assets.precompile += %w( profiles/samantha.jpg )
+Rails.application.config.assets.precompile += %w( profiles/gabe.jpg )
+Rails.application.config.assets.precompile += %w( profiles/johnton.jpg )
+Rails.application.config.assets.precompile += %w( profiles/charles.jpg )
+Rails.application.config.assets.precompile += %w( profiles/who.jpg )
